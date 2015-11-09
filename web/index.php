76ef269e7c5c0f2892b9e450078858e64ceebc03
Alex Porter <alex.porter@teamrock.com>
2015-11-09 20:38:46 +0000

    updated routes

--- web/index.php
+++ web/index.php
@@ -13,6 +13,7 @@ $kernel->loadClassCache();
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
 
 $request = Request::createFromGlobals();
 
